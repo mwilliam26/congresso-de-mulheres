987476401db9fec1c18cb93f97018583db030067
Arthur Lucas <arrtxweb@gmail.com>
2026-02-13 22:59:50 -0300

    fix: corrigir erro de digitação no título do site para o Congresso de Mulheres por causa de MW

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Cngresso de Mulheres",
+  title: "Congresso de Mulheres",
   description: "Site do Congresso de Mulheres",
 };
 
